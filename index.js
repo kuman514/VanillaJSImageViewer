5da336819b9085da36816a1e733a0e15c69581e9
kuman514 <hadjadj0@gmail.com>
2024-01-01 12:37:57 +0900

    feat: implement prev/next & fit image

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let imageIndex = 0;
 const imageInfo = [
   {
     title: '슈퍼패미콤판 펀치아웃 설명',
@@ -26,6 +27,35 @@ const imageInfo = [
  * Implement reset size/position on-change image
  */
 
+function loadImage(newImageIndex) {
+  document.querySelector('#image-here').src = `photos/${imageInfo[newImageIndex].fileName}`;
+}
+
+function handleOnClickPrev() {
+  imageIndex += imageInfo.length;
+  imageIndex--;
+  imageIndex %= imageInfo.length;
+
+  loadImage(imageIndex);
+}
+
+function handleOnClickNext() {
+  imageIndex++;
+  imageIndex %= imageInfo.length;
+
+  loadImage(imageIndex);
+}
+
+function handleOnZoomIn() {}
+
+function handleOnZoomOut() {}
+
+function init() {
+  loadImage(0);
+  document.querySelector('#prev-img-button').addEventListener('click', handleOnClickPrev);
+  document.querySelector('#next-img-button').addEventListener('click', handleOnClickNext);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#image-here').src = `photos/${imageInfo[0].fileName}`;
+  init();
 });
